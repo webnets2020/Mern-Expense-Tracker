570437ff96a0d5f1be7e467c90d2e3d7e7324d0d
chencho121 <chenchogyeltshen000@gmail.com>
2022-11-25 15:01:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [form, setForm] = useState({
+    amount: 0,
+    description: "",
+    date: "",
+  });
+
+////////////////////////////////////////////////handle input field///////////////////////////////////////////////////
+
+  const inputHandle = (e) => {
+    setForm({ ...form,[e.target.name]: e.target.value});
+    console.log(e.target.value);
+  };
+
+/////////////////////////////////////////form submit send data to backend ////////////////////////////////////////
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    // console.log(form);
+
+    const res = await fetch("http://localhost:5000/transaction",{
+      method:"POST",
+      body:form
+    });
+
+    console.log(res)
+
+
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="amount"
+          value={form.amount}
+          placeholder="Enter Transaction Amlount"
+          onChange={inputHandle}
+        />
+        <input
+          type="text"
+          name="description"
+          value={form.description}
+          placeholder="Enter Transaction Description"
+          onChange={inputHandle}
+        />
+        <input
+          type="date"
+          name="date"
+          value={form.date}
+          onChange={inputHandle}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
